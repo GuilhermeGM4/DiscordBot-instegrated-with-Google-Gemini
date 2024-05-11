a33e3d2c58739ef8016c426d0ee96f763543ad78
GuilhermeGM4 <guilherme455@pm.me>
2024-05-11 18:01:42 -0300

    fix: fixed problem where it would never list the commads

--- src/structure/CommandHandler.ts
+++ src/structure/CommandHandler.ts
@@ -4,18 +4,23 @@ import * as fs from "fs"
 export class CommandHandler{
     private configImport: any = fs.readFileSync("./src/config.json", "utf8");
     private config: any = JSON.parse(this.configImport);
-    private genAI = new GenAI(this.config.api.gemini, 'gemini-1.0-pro');
+    private genAI = new GenAI(this.config.api.gemini, 'gemini-1.5-pro-latest');
     
     constructor(){}
 
     public async handleCommand(input: string): Promise<string>{
         let inputList: string[] = input.split(" ");
-        if(inputList[1] == "help" && inputList[2] == undefined) return this.handleHelp();
+        if(inputList[0] == "help" && inputList[1] == undefined) return this.handleHelp();
 
         const command: string = inputList[0];
         inputList.shift();
         const prompt: string = inputList.join(" ");
-        let answer: string = await this.genAI.helperChat([command, prompt]);
+        let answer: string = "teste";
+        try{
+            answer = await this.genAI.helperChat([command, prompt]);
+        }catch(e){
+            console.error(e);
+        }
         return answer;
     }
 
